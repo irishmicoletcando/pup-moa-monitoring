7c5a246458717a02591ad5f1df2f358a42f9dd56
John Ric Merque <johnricmer@gmail.com>
2024-12-08 19:39:19 +0800

    feat: initialize server, db and folders

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
+const db = require('./config/db');
+
+// Specifying the path to the parent folder's .env file
+dotenv.config({ path: '../.env' });
+
+// Create the Express app
+const app = express();
+
+// Enable CORS and JSON parsing
+app.use(cors());
+app.use(express.json());
+
+// Use authentication routes
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
