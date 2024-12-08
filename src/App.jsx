8cdb0b76d70f13770ac695aebca825a4fc7012b6
Irish Micole Cando <irishmicolecando@gmail.com>
2024-12-08 16:10:54 +0800

    feat: Add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
 
 export default function App() {
   return (
-    <SignUpPage />
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUpPage />} />
+        <Route path="/log-in" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
 }
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,19 @@
+import Logo from "../components/auth/Logo"
+import WelcomeMessage from "../components/auth/WelcomeMessage"
+import Website from "../components/auth/Website"
+import LoginForm from "../components/auth/LoginForm"
+
+export default function LoginPage() {
+  return (
+    <div className="flex h-screen font-Source-Sans-Pro">
+      <div className="bg-maroon flex-1 flex flex-col justify-around space-y-40">
+        <Logo />
+        <WelcomeMessage />
+        <Website />
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
