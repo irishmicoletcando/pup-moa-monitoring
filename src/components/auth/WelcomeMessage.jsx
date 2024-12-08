31280925c789bee297f82d7031f4b60ea27569bc
Irish Micole Cando <irishmicolecando@gmail.com>
2024-12-08 14:40:58 +0800

    feat: add sign up

---
+++ src/components/auth/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+export default function WelcomeMessage() {
+  return (
+    <div className="mx-20">
+      <h1 className="text-white text-4xl font-bold mb-4">Welcome PUPian!</h1>
+      <p className="text-white">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet quam a orci aliquam tincidunt. Nullam eget lacinia est, quis iaculis risus.
+      </p>
+    </div>
+  )
+}
