c1a6b8179f0d7ab0ae63a437f78e9b18fc13375e
Irish Micole Cando <irishmicolecando@gmail.com>
2024-12-09 22:06:00 +0800

    feat: implement navbar on dashboard page

--- client/src/components/auth/Logo.jsx
+++ client/src/components/auth/Logo.jsx
@@ -1,10 +1,10 @@
-export default function Logo() {
+export default function Logo({ logoStyle, pupNameStyle, websiteNameStyle }) {
   return (
     <div className="flex mx-20">
-      <img src="/PUP.png" alt="Polytechnic University of the Philippines logo" className="h-20 mr-2" />
+      <img src="/PUP.png" alt="Polytechnic University of the Philippines logo" className={logoStyle} />
       <div className="flex flex-col justify-center ml-2">
-        <p className="text-white font-normal text-s">Polytechnic University of the Philippines - Manila</p>
-        <p className="text-white font-medium text-2xl">MOA Monitoring System</p>
+        <p className={pupNameStyle}>Polytechnic University of the Philippines - Manila</p>
+        <p className={websiteNameStyle}>MOA Monitoring System</p>
       </div>
   </div>
   )
