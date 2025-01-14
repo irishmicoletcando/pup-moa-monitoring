7bc66d84571434258bf02d62be43d82ae8fb0f37
Irish Micole Cando <irishmicolecando@gmail.com>
2025-01-14 15:51:13 +0800

    fix: Font sizes

--- client/src/components/dashboard/DashboardStats.jsx
+++ client/src/components/dashboard/DashboardStats.jsx
@@ -7,26 +7,26 @@ export default function DashboardStats() {
       {/* MOA Statistics Grid */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-x-20 gap-y-10">
         <div>
-          <h2 className="text-lg font-semibold mb-4">Employment MOA</h2>
-          <div className="bg-light-gray p-6 rounded-lg flex flex-row justify-center items-center">
+          <h2 className="text-lg font-bold mb-4">Employment</h2>
+          <div className="bg-light-gray p-6 rounded-lg flex flex-row justify-center items-center space-x-4">
             <p className="text-7xl font-bold">300+</p>
-            <p className="text-sm text-gray-600 mt-2">Number of Employment MOAs</p>
+            <p className="text-gray-600 font-semibold mt-4">Number of Employment MOAs</p>
           </div>
         </div>
 
         <div>
-          <h2 className="text-lg font-semibold mb-4">Research MOA</h2>
-          <div className="bg-light-gray p-6 rounded-lg flex flex-row justify-center items-center">
+          <h2 className="text-lg font-bold mb-4">Research</h2>
+          <div className="bg-light-gray p-6 rounded-lg flex flex-row justify-center items-center space-x-4">
             <p className="text-7xl font-bold">300+</p>
-            <p className="text-sm text-gray-600 mt-2">Number of Research MOAs</p>
+            <p className="text-gray-600 font-semibold mt-4">Number of Research MOAs</p>
           </div>
         </div>
         
         <div>
-          <h2 className="text-lg font-semibold mb-4">Practicum MOA</h2>
-          <div className="bg-light-gray p-6 rounded-lg flex flex-row justify-center items-center">
+          <h2 className="text-lg font-bold mb-4">Practicum</h2>
+          <div className="bg-light-gray p-6 rounded-lg flex flex-row justify-center items-center space-x-4">
             <p className="text-7xl font-bold">300+</p>
-            <p className="text-sm text-gray-600 mt-2">Number of Practicum MOAs</p>
+            <p className="text-gray-600 font-semibold mt-4">Number of Practicum MOAs</p>
           </div>
         </div>
       </div>
