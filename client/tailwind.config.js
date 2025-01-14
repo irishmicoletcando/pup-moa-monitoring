66d18d26fd8d32d9ba55ccd17414e038e2aab32a
Irish Micole Cando <irishmicolecando@gmail.com>
2025-01-14 15:24:44 +0800

    feat: Dashboard page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,7 +10,8 @@ export default {
         maroon: '#800000',
         red: '#AC0303',
         yellow: '#FFDF0D',
-        orange: '#DAA520'
+        orange: '#DAA520',
+        'light-gray': '#F6F6F8'
       },
       fontFamily: {
         'Source-Sans-Pro': ['Source Sans 3', 'sans-serif']
