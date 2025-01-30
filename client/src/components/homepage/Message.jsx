bd833f9ecc5b6096df765d4f7ec6d461b284c581
Angela Corpuz <angelaescariocorpuz@gmail.com>
2025-01-31 00:46:58 +0800

    feat: Add subtext

--- client/src/components/homepage/Message.jsx
+++ client/src/components/homepage/Message.jsx
@@ -1,7 +1,8 @@
 export default function Message () {
     return (
-        <div className="mx-50">
-            <h1 className="text-black text-4xl font-bold mb-4">PUP MOA Monitoring System</h1>
+        <div className="mx-50 p-4 w-[60%]">
+            <h1 className="text-red text-5xl font-bold mb-4">PUP MOA Monitoring System</h1>
+            <p className="text-lg p-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet quam a orci aliquam tincidunt. Nullam eget lacinia est, quis iaculis risus.</p>
         </div>
     )
 }
