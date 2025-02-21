d0b556c4c8af30d394b06f8199530b119c7a26bd
imcando <irishmicolecando@gmail.com>
2025-02-22 02:23:03 +0800

    chore: Move index.html to root directory

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,20 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './',
+  build: {
+    rollupOptions: {
+      external: [
+        'axios', 
+        'jszip',
+        'file-saver',
+        '@azure/storage-blob',
+        'jwt-decode',
+        'react-pdf',
+        'xlsx'
+      ],
+    },
+  },
   server: {
     proxy: {
       '/api': {
