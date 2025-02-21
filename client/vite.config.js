aea4afe05da38f5608137ebb7bc9a15b1d2065e9
imcando <irishmicolecando@gmail.com>
2025-02-22 02:30:30 +0800

    chore: Vite paths

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './',
+  publicDir: 'public',
+  build: {
+    rollupOptions: {
+      external: [
+        'axios', 
+        'jszip',
+        'file-saver',
+        '@azure/storage-blob',
+        'jwt-decode',
+        'react-pdf',
+        'xlsx'
+      ],
+    },
+  },
   server: {
+    port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
+      }
     }
-  }
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': '/src',
     },
   },
 })
