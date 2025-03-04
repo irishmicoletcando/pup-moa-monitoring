bb74bc0b4804d3ca7ba2aa8f007f0e01f8f3c25d
John Ric Merque <johnricmer@gmail.com>
2025-03-04 18:24:37 +0800

    chore: login button styling

--- client/src/components/auth/LoginForm.jsx
+++ client/src/components/auth/LoginForm.jsx
@@ -99,7 +99,16 @@ export default function LoginForm() {
 
         <button
           type="submit"
-          className="bg-maroon text-white font-bold mt-5 py-2 px-4 w-full hover:bg-red-700 text-lg md:text-base"
+          className="font-bold mt-5 w-full hover:bg-red-700 text-lg md:text-base
+                    inline-flex items-center justify-center gap-2 
+                    bg-maroon text-white 
+                    px-3 py-2 rounded-md
+                    shadow-sm
+                    hover:bg-red hover:shadow-md
+                    active:bg-maroon active:scale-95
+                    transition-all duration-200 ease-in-out
+                    border border-maroon
+                    focus:outline-none focus:ring-2 focus:ring-maroon focus:ring-offset-2"
         >
           Login
         </button>
